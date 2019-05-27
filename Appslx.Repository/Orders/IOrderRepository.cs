c7eb37521d3f3281d6ce68a9ac4e48f0c2b0c69f
codelicious <franzkenny26995@gmail.com>
2019-05-27 07:15:00 +0700

    OrderRepo

---
+++ Appslx.Repository/Orders/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Appslx.Core.Models;
+using Appslx.Repository.Base;
+
+namespace Appslx.Repository.Orders
+{
+    public interface IOrderRepository:IGenericRepository<Order>
+    {
+        Order GetById(int id);
+    }
+}
